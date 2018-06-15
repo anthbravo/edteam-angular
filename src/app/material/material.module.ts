c6d817e25329bbed0d66253770c97612b87a7d9e
anthbravo <anthbravo@gmail.com>
2018-06-14 19:21:09 -0500

    Se implementa el modulo MatButtonModule. Este nos permitira usar la directiva mat-icon-button, que da estilo visual de cambio de cursor, forma y efecto de click

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from "@angular/common";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatToolbarModule } from "@angular/material";
+import { MatToolbarModule, MatButtonModule } from "@angular/material";
 
 import { MaterialDemoComponent } from "./material-demo/material-demo.component";
 
 @NgModule({
-  imports: [CommonModule, BrowserAnimationsModule, MatToolbarModule],
-  exports: [MatToolbarModule],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  exports: [MatToolbarModule, MatButtonModule],
   declarations: [MaterialDemoComponent]
 })
 export class MaterialModule {}
