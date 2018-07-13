9e41034c04956d686cda6e15eba77048bff3afb5
anthbravo <anthbravo@gmail.com>
2018-07-13 11:49:33 -0500

    Se crea un modelo de datos basado en Estudiante. Estudiante es una interface creado en un apartado shared/model/Estudiante.ts. Este modelo nos ayudara a poder manejar la información de usuario de forma dinamica, logrando asi interacción entre el template y la logica TS

--- src/app/material/material-list/material-list.component.ts
+++ src/app/material/material-list/material-list.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Estudiante } from "../../shared/model/Estudiante";
 
 @Component({
-  selector: 'ed-material-list',
-  templateUrl: './material-list.component.html',
-  styleUrls: ['./material-list.component.css']
+  selector: "ed-material-list",
+  templateUrl: "./material-list.component.html",
+  styleUrls: ["./material-list.component.css"]
 })
 export class MaterialListComponent implements OnInit {
-
-  constructor() { }
+  estudiante1: Estudiante;
+  estudiante2: Estudiante;
+  estudiante3: Estudiante;
+  constructor() {}
 
   ngOnInit() {
+    console.log("Inicializando el Componente MaterialList");
+    this.estudiante1 = {
+      id: 1,
+      nombre: "Juan Ramirez",
+      ciudad: "Lima"
+    };
+    this.estudiante2 = {
+      id: 2,
+      nombre: "Jose Bustamante",
+      ciudad: "Nazca"
+    };
+    this.estudiante3 = {
+      id: 3,
+      nombre: "Luis Ibañez",
+      ciudad: "Apurimac"
+    };
   }
-
 }
